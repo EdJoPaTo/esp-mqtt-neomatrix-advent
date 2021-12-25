4a2f83d6b8f1d9561540c794b042e0970ece3138
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-12-25 14:25:30 +0100

    style: autoformat

--- esp-mqtt-neomatrix-advent.ino
+++ esp-mqtt-neomatrix-advent.ino
@@ -184,42 +184,42 @@ uint16_t ColorHSV(uint16_t hue, uint8_t sat, uint8_t val) {
   hue = (hue * 1530L + 32768) / 65536;
 
   // Convert hue to R,G,B (nested ifs faster than divide+mod+switch):
-  if(hue < 510) {         // Red to Green-1
+  if (hue < 510) {         // Red to Green-1
     b = 0;
-    if(hue < 255) {       //   Red to Yellow-1
+    if (hue < 255) {       //   Red to Yellow-1
       r = 255;
-      g = hue;            //     g = 0 to 254
-    } else {              //   Yellow to Green-1
-      r = 510 - hue;      //     r = 255 to 1
+      g = hue;             //     g = 0 to 254
+    } else {               //   Yellow to Green-1
+      r = 510 - hue;       //     r = 255 to 1
       g = 255;
     }
-  } else if(hue < 1020) { // Green to Blue-1
+  } else if (hue < 1020) { // Green to Blue-1
     r = 0;
-    if(hue <  765) {      //   Green to Cyan-1
+    if (hue < 765) {       //   Green to Cyan-1
       g = 255;
-      b = hue - 510;      //     b = 0 to 254
-    } else {              //   Cyan to Blue-1
-      g = 1020 - hue;     //     g = 255 to 1
+      b = hue - 510;       //     b = 0 to 254
+    } else {               //   Cyan to Blue-1
+      g = 1020 - hue;      //     g = 255 to 1
       b = 255;
     }
-  } else if(hue < 1530) { // Blue to Red-1
+  } else if (hue < 1530) { // Blue to Red-1
     g = 0;
-    if(hue < 1275) {      //   Blue to Magenta-1
-      r = hue - 1020;     //     r = 0 to 254
+    if (hue < 1275) {      //   Blue to Magenta-1
+      r = hue - 1020;      //     r = 0 to 254
       b = 255;
-    } else {              //   Magenta to Red-1
+    } else {               //   Magenta to Red-1
       r = 255;
-      b = 1530 - hue;     //     b = 255 to 1
+      b = 1530 - hue;      //     b = 255 to 1
     }
-  } else {                // Last 0.5 Red (quicker than % operator)
+  } else {                 // Last 0.5 Red (quicker than % operator)
     r = 255;
     g = b = 0;
   }
 
   // Apply saturation and value to R,G,B
-  uint32_t v1 =   1 + val; // 1 to 256; allows >>8 instead of /255
-  uint16_t s1 =   1 + sat; // 1 to 256; same reason
-  uint8_t  s2 = 255 - sat; // 255 to 0
+  uint32_t v1 = 1 + val;  // 1 to 256; allows >>8 instead of /255
+  uint16_t s1 = 1 + sat;  // 1 to 256; same reason
+  uint8_t s2 = 255 - sat; // 255 to 0
 
   r2 = ((((r * s1) >> 8) + s2) * v1) >> 8;
   g2 = ((((g * s1) >> 8) + s2) * v1) >> 8;
